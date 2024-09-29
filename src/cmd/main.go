a7ed3b5b2804aee14ddfc5010484dd937f1b5acb
IX <borntokill.ix@gmail.com>
2024-09-29 23:42:36 +0700

    feature(main): add migrations with startup server

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+    "SongsLibrary/src/internal/databases/migrations"
+    "SongsLibrary/src/pkg/configs"
     "SongsLibrary/src/server"
     v1 "SongsLibrary/src/server/routers/v1"
 )
-import "SongsLibrary/src/pkg/configs"
 
 func main() {
     settings := *configs.NewServerSettings()
 
+    migrations.RunMigrations()
+
+    // Start Server
     s := server.NewServer(settings)
     s.Start()
     s.AddRouter(v1.NewSongRouters())
